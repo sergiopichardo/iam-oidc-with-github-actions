7fa105afe124f711e5f8a247c2a6a8f80b7e008a
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-11-16 12:05:46 -0600

    Initial commit

---
+++ test/iam-oidc-with-github-actions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IamOidcWithGithubActions from '../lib/iam-oidc-with-github-actions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iam-oidc-with-github-actions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IamOidcWithGithubActions.IamOidcWithGithubActionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
